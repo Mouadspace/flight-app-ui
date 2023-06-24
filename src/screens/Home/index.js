9c12115472fc833bd7ec148f47aec8ea8af9aec8
mouad <mouadsaif.ms@gmail.com>
2023-06-24 20:30:15 +0100

    init

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import styles from './style';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>hello mouad</Text>
+    </SafeAreaView>
+  );
+};
+
+export default React.memo(Home);
